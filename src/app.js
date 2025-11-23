7b968141bce5edf6fe4a467464f12e08d07a965f
OsmarAraujoDev <osmar.araujo.dev@gmail.com>
2025-11-23 19:28:02 -0300

    feature/add cors e helmet no app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const userRoutes = require('./routes/user.routes');
 const productRoutes = require('./routes/product.routes');
 const orderRoutes = require('./routes/order.routes');
 
 const app = express();
+
+app.use(helmet());
+
+app.use(cors({
+  origin: [process.env.ORIGIN_1, process.env.ORIGIN_2],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
